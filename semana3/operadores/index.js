33e38451eced007a64083de615abdadc8b44762b
pablosilas <pablosilas14@gmail.com>
2020-12-01 16:00:09 -0300

    Realizei todos os exericicios de javascript, estão todos comentados

---
+++ semana3/operadores/index.js
@@ -0,0 +1,60 @@
+//EXERCICIO DE INTERPRETAÇÃO 1: a.false, b.false, c.true, e.boolean
+
+//
+//
+
+//EXERCICIO DE INTERPRETAÇÃO 2: a.undefined, b.null, c.11, d.3, e.3,19,5,6,7,8,9,10,11,12,13, f.9
+
+//
+//
+
+//EXERCICIO 1
+
+let idade = prompt("Qual sua idade?")
+let idade2 = prompt("Qual a idade do seu melhor amigo?")
+const maior = Number(idade) > Number(idade2)
+console.log ("Sua idade é maior do que a do seu melhor aamigo?", maior)
+const diferenca = Number(idade) - Number(idade2)
+console.log ("A diferença de idades é :", diferenca)
+
+//
+//
+
+//EXERCICIO2
+
+// const par = prompt("Insira um numero par:")
+// const resto = Number(par)%2
+// console.log ("O resto da divisão é:", resto)
+//TODO NUMERO PAR O RESTO É = 0, E SE INSERIR UM NUMERO IMPAR O RESTO É = 1
+
+//
+//
+
+//EXERCICIO3
+
+// let listaDeTarefas = []
+// const tarefa1 = prompt("Insira uma primeira tarefa:")
+// listaDeTarefas.push(tarefa1)
+// const tarefa2 = prompt("Insira uma segunda tarefa:")
+// listaDeTarefas.push(tarefa2)
+// const tarefa3 = prompt("Insira uma terceira tarefa:")
+// listaDeTarefas.push(tarefa3)
+
+// console.log (listaDeTarefas)
+
+// const remove = prompt("Digite um indice 0, 1, 2:")
+// listaDeTarefas.splice(remove, 1)
+
+// console.log(listaDeTarefas)
+
+//
+//
+
+//EXERCICIO4
+
+// const emailDoUsuario = prompt("Qual o seu e-mail:")
+// const nomeDoUsuario = prompt("Qual o nome do seu usuario:")
+// console.log ("O e-mail:", emailDoUsuario, "foi cadastrado com sucesso. Seja bem vindo(a)", nomeDoUsuario)
+
+//
+//
