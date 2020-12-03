f2ce1b520af833f02760ffe9708921c6178852eb
pablosilas <pablosilas14@gmail.com>
2020-12-03 15:39:55 -0300

    Fiz todos os exercicio de JS e estão todos comentados

---
+++ semana3/loops/index.js
@@ -0,0 +1,49 @@
+//EXERCICIO 1  - O Valor é 10. No exericico a variavel "i" recebe +1 até chegar no valor menor que 5, no caso o numero 4, depois a variavel valor recebe o valor do "i"
+
+//EXERCICIO 2 - A. Imprimem os valores maiores que 18. B. A variavel "numero" possui a informação dos indice do array
+
+//EXERCICiO 3
+const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+//A.
+
+for(num = 0; num < 12; num++){
+    console.log(array[num])
+}
+
+//B.
+
+// for(num = 0; num < array.length; num++){
+//     divisao = array[num] / 10
+//     console.log(divisao)
+// }
+
+//C.
+
+//  let par = []
+//  for(num of array){
+//     if(num % 2 === 0){
+//     par = num
+//     }
+//     console.log(par)
+//  }
+
+//D.
+
+// for(i = 0; i < 12; i++){
+//     console.log("O elemento do index:", i, "é", array[i]) 
+// }
+
+//E
+
+// let maior = 0
+// let menor = array[0]
+// for (num of array){ 
+//     if(num > maior){
+//         maior = num 
+//     }
+//     if(num < menor) {
+//         menor = num
+//     }
+// }
+// console.log("O maior número é: ", maior, "e o menor é: ", menor)
