f3c1dac604cd1242b6b3c1e6278a4cb2411b82bf
pablosilas <pablosilas14@gmail.com>
2020-12-11 15:26:25 -0300

    Conclui o exercicio de lógica de programacao

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,48 @@
+//EXERCICIO DE LÓGICA DE PROGRAMAÇÃO
+
+//1) PRIMEIRA FORMA DE PERCORRER ARRAY
+
+// const numeros = [13, 20, 54, 32, 49]
+
+// const posicaoNumero = numeros.map((item, index, array) =>{
+//     return `O numero ${item} está na posição ${index}`
+// })
+// console.log(posicaoNumero)
+
+//SEGUNDA FORMA DE PERCORRER ARRAY
+
+// const numeros = [13, 20, 54, 32, 49]
+// for(x of numeros) {
+//     if(x % 2 ===0){
+//         console.log("Os numeros pares são: ", x)
+//     }
+// }
+
+//TERCEIRA FORMA DE PERCORRER ARRAY
+
+// const numeros = [13, 20, 54, 32, 49]
+
+// numeros.forEach(item => {
+//     if(item % 2 !== 0) {
+//         console.log("Os numeros impares são: ", item)
+//     }
+// });
+
+//2) 
+//a) - false 
+//b) - false
+//c) - true
+//d) - true
+//e) - true
+
+//
+//
+
+//EXERCICIO DE FUNÇÕES
+
+
+
+
+
+
+
