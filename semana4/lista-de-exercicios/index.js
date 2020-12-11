2ca850d017d02e4c46052b6532e03b3b31a73f8e
pablosilas <pablosilas14@gmail.com>
2020-12-11 16:31:58 -0300

    Conclui o exercicio de obejtos

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -40,6 +40,65 @@
 
 //EXERCICIO DE FUNÇÕES
 
+//1)
+// const array = [13, 20, 54, 32, 49]
+
+// const maiorMenor = (array) => {
+
+//     let maior = 0
+//     let menor = numeros[0]
+
+//     for(item of array){
+//         if(item < maior){
+//             maior = item
+//         }
+//     }   if(item > menor){
+//         menor = item
+//     }
+
+// }
+// console.log(maior, menor)
+// console.log(array)
+
+
+//2)
+// const msg = () => {
+//     alert("Hello Labenu!")
+// }
+// msg()
+
+//EXERCICIO DE OBJETOS 
+//1 - O OBJETO É UMA FORMA DE ORGANIZAR MELHOR AS INFORMÇÕES QUE ESTÃO DENTRO DE UM ARRAY, PODENDO SEPARAR E FICAR MAIS CLARO A COMPREENSÃO.
+
+//2)
+// const criaRetrangulo = (lado1, lado2) => {
+//     const retangulo = {
+//         largura : (lado1) ,
+//         altura : (lado2) ,
+//         perimetro : (2*(lado1 + lado2)) ,
+//         area : (lado1 * lado2) 
+//     }
+//     console.log(retangulo)
+   
+// }
+// criaRetrangulo(23, 44)
+
+//3)
+// const filme = {
+//     titulo: "As Branquelas",
+//     ano : "2004",
+//     diretor: "Keenan Ivory Wayans" ,
+//     ator1: "Shawn Wayans",
+//     ator2: "Marlon Wayans"
+// }
+
+// console.log(`Venha assistir ${filme.titulo} de ${filme.ano} dirigido por ${filme.diretor} e estrelado por ${filme.ator1} e ${filme.ator2}`)
+
+//4)
+const pessoa = {
+    
+}
+
 
 
 
